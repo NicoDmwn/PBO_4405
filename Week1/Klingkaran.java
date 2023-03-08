d4796a174e70f94d00ef372a580f732508a8cf51
NicoDmwn <nicod432@gmail.com>
2023-03-08 21:57:26 +0700

    Week1_PBO

---
+++ Week1/Klingkaran.java
@@ -0,0 +1,9 @@
+public class Klingkaran {
+    public static void main(String[] args) {
+       double diameter = 10;
+       double jariJari = diameter/2; 
+       double keliling = 2 * Math.PI * jariJari; // menghitung keliling lingkaran
+       System.out.println("Keliling lingkaran dengan jari-jari 10 cm adalah " + keliling + " cm");
+    }
+ }
+ 
