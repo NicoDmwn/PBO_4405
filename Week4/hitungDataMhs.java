426375346e95f74d61dfed21c9da8fb66f7344f4
NicoDmwn <91961521+NicoDmwn@users.noreply.github.com>
2023-03-27 22:12:28 +0700

    Delete hitungDataMhs.java

--- Week4/hitungDataMhs.java
+++
@@ -1,7 +0,0 @@
-public class hitungDataMhs{
-    public static void main(String args[]){
-        DataMhs mhs1 = new DataMhs();
-        mhs1.Datanilai("A11.2021.13422", "Zacky", 95, 85 ,80);
-        mhs1.tampilData();
-    }
-}
